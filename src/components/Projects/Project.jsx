af43d5982ed2717a2fbf758e9eb2c66c1887dca6
Rocky Chowdhury <rocky20809@gmail.com>
2025-01-06 21:59:11 +0600

    resume download button added

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,15 +1,15 @@
 import { Github,ArrowUpRight } from 'lucide-react';
 const Project = ({ project }) => {
     return (
-        <div className="bg-marble rounded-2xl p-5 space-y-2">
+        <div className="bg-secondary/40 hover:bg-secondary rounded-2xl p-5 space-y-2 shadow-md hover:scale-105 transition">
             <figure className="w-full">
                 <img className="w-full min-h-72  rounded-2xl" src={project.preview} alt="preview" />
             </figure>
             <div className='flex items-center justify-between'>
                 <h3 className="font-medium text-xl">{project.name}</h3>
                 <div className='px-5 flex items-center gap-4'>
-                    <p className='bg-natural rounded-full p-1 text-slate-200'><a target='_blank' href={project.github}><Github size={25} /></a></p>
-                    <p className='bg-action rounded-full p-1 text-slate-200'><a target='_blank' href={project.live}><ArrowUpRight size={25} /></a></p>
+                    <p className='bg-natural rounded-full p-1 text-slate-200  shadow-md hover:scale-105 transition'><a target='_blank' href={project.github}><Github size={25} /></a></p>
+                    <p className='bg-action rounded-full p-1 text-slate-200  shadow-md hover:scale-105 transition'><a target='_blank' href={project.live}><ArrowUpRight size={25} /></a></p>
                 </div>
             </div>
             <h6>{project.type}</h6>
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { toast } from 'react-toastify';
-
+import { CircleArrowDown } from 'lucide-react';
 const Navbar = () => {
     const textRef = useRef();
     const copyToClipboard = () => {
@@ -16,15 +16,18 @@ const Navbar = () => {
                 });
         }
     };
+    const resumeLink ="https://drive.google.com/uc?export=download&id=1zK5LxNeAA7_JdWgvgICkm-c-lkrZOSlF";
     return (
         <div className="py-8 flex items-center justify-between">
             <div className="flex gap-8">
                 <div className="flex border rounded-full w-fit items-center drop-shadow-sm ">
-                    <h2  ref={textRef}  className="font-medium text-lg px-4">rocky20809@gmail.com</h2>
-                    <button  onClick={copyToClipboard}  className="bg-marble hover:scale-105 transition-transform hover:border-action border border-transparent px-6 py-2 rounded-full font-semibold" >Copy</button>
+                    <h2 ref={textRef} className="font-medium text-lg px-4">rocky20809@gmail.com</h2>
+                    <button onClick={copyToClipboard} className="bg-marble hover:scale-105 transition-transform hover:border-action border border-transparent px-6 py-2 rounded-full font-semibold" >Copy</button>
                 </div>
                 <div className="">
-                    <button className="bg-marble px-6 py-2 rounded-full font-semibold" >Resume</button>
+                    <button onClick={() => {
+                        window.open(resumeLink, "_blank");
+                    }} className="bg-action hover:scale-105 transition shadow-md text-marble border-action border px-6 py-2 flex items-center gap-3 rounded-full font-semibold" >Resume <CircleArrowDown size={25} /></button>
                 </div>
             </div>
             <div>
